8f5e2f4a67ebda813478a70f82a53f879d3e18a3
Tiago Villaca <60266341+villaca13@users.noreply.github.com>
2025-03-11 12:42:09 +0000

    changes on the navigation bar to show active page.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("videos");
-  };
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+};
   
